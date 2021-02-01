7e9a0c5a1894171ae9a5131e50c0de9d0885df69
brenu <bvsousa64@gmail.com>
2021-01-31 23:16:51 -0300

    :rocket: Create Peer Server and its main events

--- public/pages/room/src/app.js
+++ public/pages/room/src/app.js
@@ -16,6 +16,17 @@ const onload = () => {
 
   const socketUrl = "http://localhost:3000";
   const socketBuilder = new SocketBuilder({ socketUrl });
+
+  const peerConfig = Object.values({
+    id: undefined,
+    config: {
+      port: 9000,
+      host: "localhost",
+      path: "/",
+    },
+  });
+  const peerBuilder = new PeerBuilder({ peerConfig });
+
   const view = new View();
   const media = new Media();
 
@@ -24,6 +35,7 @@ const onload = () => {
     media,
     room,
     socketBuilder,
+    peerBuilder,
   };
 
   Business.initialize(deps);
